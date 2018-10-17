04f83d3fe95ebfb80aa39a3a5647e8ae97a40c7f
Koljasha <koljasha@mail.ru>
2018-10-17 19:27:27 +0300

    bootstrap grid

--- local/src/script.js
+++ local/src/script.js
@@ -1,6 +1,5 @@
-// import Vue from 'vue';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
 import './style.css';
 
 import Particles from 'particlesjs';
@@ -14,24 +13,3 @@ window.onload = function() {
     color: '#0082EE',
   });
 };
-
-// import seasonvar from './main';
-
-// const app = new Vue({
-//     el: '#app',
-//     data: {
-//         url: '',
-//         error_: false
-//     },
-//     methods: {
-//         openHD: () => {
-//             try {
-//                 seasonvar(app.url);
-//                 app.error_ = false;
-//             } catch (err) {
-//                 app.error_ = true;
-//             }
-
-//         }
-//     }
-// })
